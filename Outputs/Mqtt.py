3ce5ed8af52d4750c370643dc634ae3aadf77633
Petr Hovorka <19402950+hovorkap@users.noreply.github.com>
2023-07-18 17:11:05 +0200

    kjlk

--- Outputs/Mqtt.py
+++ Outputs/Mqtt.py
@@ -23,7 +23,7 @@ class Mqtt(object):
         inverterDetails.pop('name', None)
         inverterDetails.pop('#SolaxClient', None)
 
-        name = inverterDetails.items()['name']
+        name = inverterDetails.pop('name', None)
         
         sys.stdout.write(f"{self.mqtt_topic}/{name}/{inverterDetails}")
         # for x, y in inverterDetails.items():
